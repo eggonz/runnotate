ce98e2c155011f1659ba0f4b81b4c08efb0aa22f
eggonz <egoitz.gonz@gmail.com>
2023-01-18 14:26:59 +0100

    show annotations with color

--- runnotate/__main__.py
+++ runnotate/__main__.py
@@ -1,5 +1,4 @@
 import argparse
-import itertools
 import json
 import os
 import re
@@ -8,73 +7,87 @@ import cv2
 import pandas as pd
 
 WINDOW_NAME = 'runnotate'
-DEFAULT_CONFIG = 'config.json'
+DEFAULT_CONFIG_PATH = 'config.json'
+DEFAULT_LABEL_COLOR = (0, 0, 0)
+
+ASCII = {
+    '0': 48,
+    '1': 49,
+    '2': 50,
+    '3': 51,
+    '4': 52,
+    '5': 53,
+    '6': 54,
+    '7': 55,
+    '8': 56,
+    '9': 57,
+
+    'a': 97,
+    'b': 98,
+    'c': 99,
+    'd': 100,
+    'e': 101,
+    'f': 102,
+    'g': 103,
+    'h': 104,
+    'i': 105,
+    'j': 106,
+    'k': 107,
+    'l': 108,
+    'm': 109,
+    'n': 110,
+    'o': 111,
+    'p': 112,
+    'q': 113,
+    'r': 114,
+    's': 115,
+    't': 116,
+    'u': 117,
+    'v': 118,
+    'w': 119,
+    'x': 120,
+    'y': 121,
+    'z': 122,
+
+    'Backspace': 8,
+    'Tab': 9,
+    'Enter': 13,
+    'Esc': 27,
+    'Spacebar': 32,
+    'Delete': 127,
+}
+
+
+def color_hex2bgr(hex_):
+    hex_ = hex_.lstrip('#')
+    lv = len(hex_)
+    rgb = tuple(int(hex_[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+    bgr = tuple(reversed(rgb))
+    return bgr
 
 
 class Config:
-    key2int = {
-        '0': 48,
-        '1': 49,
-        '2': 50,
-        '3': 51,
-        '4': 52,
-        '5': 53,
-        '6': 54,
-        '7': 55,
-        '8': 56,
-        '9': 57,
-
-        'a': 97,
-        'b': 98,
-        'c': 99,
-        'd': 100,
-        'e': 101,
-        'f': 102,
-        'g': 103,
-        'h': 104,
-        'i': 105,
-        'j': 106,
-        'k': 107,
-        'l': 108,
-        'm': 109,
-        'n': 110,
-        'o': 111,
-        'p': 112,
-        'q': 113,
-        'r': 114,
-        's': 115,
-        't': 116,
-        'u': 117,
-        'v': 118,
-        'w': 119,
-        'x': 120,
-        'y': 121,
-        'z': 122,
-
-        'Backspace': 8,
-        'Tab': 9,
-        'Enter': 13,
-        'Esc': 27,
-        'Spacebar': 32,
-        'Delete': 127,
-    }
-
     def __init__(self, path, *, data='', out=''):
         with open(path, 'r') as file:
-            self.config = json.load(file)
+            config_file = json.load(file)
 
-        for field in ('labels', 'controls'):
-            for label in self.config[field]:
-                key_codes = self.config[field][label]
-                self.config[field][label] = [Config.key2int.get(code, -1) for code in key_codes]
+        self._control_keys = {}
+        for control in config_file['controls']:
+            key_codes = config_file['controls'][control]
+            self._control_keys[control] = [ASCII.get(code, -1) for code in key_codes]
 
-        self.int2label = {}
-        for label, key_list in self.config['labels'].items():
-            for key in key_list:
-                self.int2label[key] = label
+        self._label_colors = {}
+        for label in config_file['labels']:
+            color = config_file['labels'][label]['color']
+            self._label_colors[label] = color_hex2bgr(color)
 
-        self._data = self.config['data'] if not data else data
-        self._out = self.config['out'] if not out else out
+        self._int2label = {}
+        for label, keys_color in config_file['labels'].items():
+            for key in keys_color['keys']:
+                self._int2label[ASCII.get(key, -1)] = label
+
+        self._data = config_file['data'] if not data else data
+        self._out = config_file['out'] if not out else out
 
     @property
     def data(self):
@@ -86,22 +99,25 @@ class Config:
 
     @property
     def label_keys(self):
-        return itertools.chain(*self.config['labels'].values())
+        return self._int2label.keys()
 
     @property
     def quit_keys(self):
-        return self.config['controls']['quit']
+        return self._control_keys.get('quit', -1)
 
     @property
     def next_keys(self):
-        return self.config['controls']['next']
+        return self._control_keys.get('next', -1)
 
     @property
     def back_keys(self):
-        return self.config['controls']['back']
+        return self._control_keys.get('back', -1)
 
     def get_key_label(self, key_num):
-        return self.int2label[key_num]
+        return self._int2label[key_num]
+
+    def get_label_color(self, label):
+        return self._label_colors.get(label, DEFAULT_LABEL_COLOR)
 
 
 class SaveFile:
@@ -131,7 +147,7 @@ class SaveFile:
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", default=DEFAULT_CONFIG, type=str,
+    parser.add_argument("--config", default=DEFAULT_CONFIG_PATH, type=str,
                         help='path to config file (default: "./config.json")')
     parser.add_argument("--data", default='', type=str,
                         help='path to directory containing images, overrides value in config')
@@ -164,7 +180,6 @@ def load():
 
 
 def run(config, sav, img_list, df):
-
     cv2.namedWindow(WINDOW_NAME)
     cv2.startWindowThread()
 
@@ -181,6 +196,19 @@ def run(config, sav, img_list, df):
         filepath = os.path.join(config.data, filename)
 
         img = cv2.imread(filepath)
+
+        curr_label = None
+        try:
+            curr_label = df.loc[img_id]
+        except KeyError:
+            pass
+
+        if curr_label is not None:
+            curr_label = curr_label['label']
+            height, width, _ = img.shape
+            color = config.get_label_color(curr_label)
+            cv2.rectangle(img, (3, 3), (width - 3, height - 3), color, 6)
+
         cv2.imshow(WINDOW_NAME, img)
 
         key = cv2.waitKey(1)
